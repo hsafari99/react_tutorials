bee84b33e89eddcfd9a96e1ac7a3921e7d0a2708
hsafari99 <hsafari99@gmail.com>
2023-07-28 17:56:20 -0400

    Fix routing issue

--- moduleFederation_React/marketing/src/App.js
+++ moduleFederation_React/marketing/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 // Using Routr instead of Browser Router because we don't need history (contains browsing history) all over the place.
 // The only place we need it is in container. the rest (which are normally MFEs) need the currect url which is available in Router component
 // Router using memory history not browser history. it will help keep browser history unique and integrated between all repos
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-export default ({ history }) => { console.warn("HISTORY: ", history);
-    return <Router history={history} >
+export default () => {
+    return <BrowserRouter>
                 <Switch>
-                    <Route path="/pricing" element={ <Pricing /> } />
-                    <Route path="/" element={ <Landing /> } />
+                    <Route exact path="/"><Landing/></Route>
+                    <Route path="/pricing"><Pricing/></Route>
+                    <Route path="*"><div>NOT FOUND</div></Route>
                 </Switch>
-            </Router>;
+            </BrowserRouter>;
 }
